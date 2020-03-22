e8b913c06a6eecf0fa25a3108bf5eb6ee6f0f058
Grant Smith <grantnsmith@gmail.com>
2020-03-22 16:52:04 -0700

    Built array and adjusted quiz card function to pull from array questions and answers.

--- script.js
+++ script.js
@@ -36,23 +36,50 @@ var answer1 = document.querySelector("#answer1");
 var answer2 = document.querySelector("#answer2");
 var answer3 = document.querySelector("#answer3");
 var answer4 = document.querySelector("#answer4");
+var questionBtn = document.querySelector(".questionBtn");
 
 // Quiz card function
 
 function quizQuestionFunct() {
     quizCard.style.visibility = "visible";
-    questionNumber.textContent = "";
-    quizQuestion.textContent = "";
-    answer1.textContent = "";
-    answer2.textContent = "";
-    answer3.textContent = "";
-    answer4.textContent = "";
+    // for (i = 0; i < qAndAArr.length; i++) {
+    
+    questionNumber.textContent = qAndAArr[0].questionNumber;
+    quizQuestion.textContent = qAndAArr[0].question;
+    answer1.textContent = qAndAArr[0].answer1;
+    answer2.textContent = qAndAArr[0].answer2;
+    answer3.textContent = qAndAArr[0].answer3;
+    answer4.textContent = qAndAArr[0].answer4;
+    
+    }
+    // Still testing correct answer mechanism
+    // if (qAndAArr[0].answer === answer2) {
+        
+    // }
 
 } 
-
 // Quiz array of questions and answers
 
-var questionsAndAnswersArr = []
+var qAndAArr = [
+    {
+        questionNumber: "Question 1",
+        question: "Quiz Question Number 1?",
+        answer1: "This is answer 1",
+        answer2: "This is answer 2",
+        answer3: "This is answer 3",
+        answer4: "This is answer 4",
+        answer: answer2,
+    },
+    {
+        questionNumber: "Question 2",
+        question: "Quiz Question Number 2?",
+        answer1: "This is answer 1",
+        answer2: "This is answer 2",
+        answer3: "This is answer 3",
+        answer4: "This is answer 4",
+    }
+]
+
 
 //   End of Game function
 
@@ -61,3 +88,8 @@ var questionsAndAnswersArr = []
   }
 
 startBtnEl.addEventListener("click", startQuiz);
+
+
+// Unsure on where to put this event listener
+// questionBtn.addEventListener("click", quizQuestionFunct);
+
