104c4a0d7fe9f94720dd355d6937f9f415ce3f57
Grant Smith <grantnsmith@gmail.com>
2020-03-22 15:49:22 -0700

    Added quiz card variables and function for creating quiz card based on array (building next)

--- script.js
+++ script.js
@@ -20,10 +20,39 @@ var startBtnEl = document.querySelector("#startBtn");
 var welcMessageEl = document.querySelector("#welcomeMessage");
 
 
-function startGame() {
+function startQuiz() {
     welcMessageEl.style.display = "none";
     setTime();
+    quizQuestionFunct();
+
 }
+
+// Quiz question variables
+
+var quizCard = document.querySelector("#quizCard");
+var questionNumber = document.querySelector("#quizCardTitle");
+var quizQuestion = document.querySelector("#quizCardText");
+var answer1 = document.querySelector("#answer1");
+var answer2 = document.querySelector("#answer2");
+var answer3 = document.querySelector("#answer3");
+var answer4 = document.querySelector("#answer4");
+
+// Quiz card function
+
+function quizQuestionFunct() {
+    quizCard.style.visibility = "visible";
+    questionNumber.textContent = "";
+    quizQuestion.textContent = "";
+    answer1.textContent = "";
+    answer2.textContent = "";
+    answer3.textContent = "";
+    answer4.textContent = "";
+
+} 
+
+// Quiz array of questions and answers
+
+var questionsAndAnswersArr = []
 
 //   End of Game function
 
@@ -31,4 +60,4 @@ function startGame() {
       timeEl.textContent = "Game Over";
   }
 
-startBtnEl.addEventListener("click", startGame);
+startBtnEl.addEventListener("click", startQuiz);
