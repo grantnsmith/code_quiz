fe18184e76c0b8a81611c3a39b7db03f97ec1ea8
Grant Smith <grantnsmith@gmail.com>
2020-03-25 15:24:18 -0700

    Deleted commented out code that didn't work, moved things around for better formatting of code.

--- script.js
+++ script.js
@@ -51,12 +51,10 @@ var qAndAArr = [
 var startBtnEl = document.querySelector("#startBtn");
 var welcMessageEl = document.querySelector("#welcomeMessage");
 
-
 function startQuiz() {
     welcMessageEl.style.display = "none";
     setTime();
     quizQuestionFunct();
-
 }
 
 // Timer
@@ -67,12 +65,10 @@ function setTime() {
     var timerInterval = setInterval(function() {
       secondsLeft--;
       timeEl.textContent = "Time: " + secondsLeft;
-  
       if(secondsLeft < 0 || currentQuestion === qAndAArr.length) {
         clearInterval(timerInterval);
         endGame();
       }
-  
     }, 1000);
   }
 
@@ -85,7 +81,6 @@ var answer1 = document.querySelector("#answer1");
 var answer2 = document.querySelector("#answer2");
 var answer3 = document.querySelector("#answer3");
 var answer4 = document.querySelector("#answer4");
-// var questionBtn = document.querySelectorAll(".questionBtn");
 var answer1Btn = document.querySelector("answer1");
 var answer2Btn = document.querySelector("answer2");
 var answer3Btn = document.querySelector("answer3");
@@ -105,7 +100,6 @@ function quizQuestionFunct() {
     answer2.textContent = qAndAArr[currentQuestion].answers.b;
     answer3.textContent = qAndAArr[currentQuestion].answers.c;
     answer4.textContent = qAndAArr[currentQuestion].answers.d;
- 
 } 
 
 // Check answer function
@@ -116,18 +110,14 @@ function checkAnswer() {
   } else {
     answerMessage.textContent = "Wrong Answer!"
   }
-
-  nextQuestionBtnEl.style.visibility = "visible";
-    
+  nextQuestionBtnEl.style.visibility = "visible";  
 }
 // Next question function
 
 function nextQuestionFunc() {
   currentQuestion++;
     quizQuestionFunct();
-  
   }
-
 
 //   End of Game function
 
@@ -138,7 +128,7 @@ function nextQuestionFunc() {
 // Event listeners
 
 startBtnEl.addEventListener("click", startQuiz);
-
+nextQuestionBtnEl.addEventListener("click", nextQuestionFunc);
 quizCard.addEventListener("click", function(event){
   if (event.target.id === "answer1" ||
       event.target.id === "answer2" ||
@@ -149,5 +139,3 @@ quizCard.addEventListener("click", function(event){
     return;
   }
 });
-
-nextQuestionBtnEl.addEventListener("click", nextQuestionFunc);
